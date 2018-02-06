a12afe70793a30c8aac478245bd956a448b3b9a8
rjaesguerra <rjaesguerra@gmail.com>
2018-02-05 21:40:26 -0800

    Added new SeleniumTest project

---
+++ SeleniumTest/src/main/java/project/SeleniumTest/App.java
@@ -0,0 +1,13 @@
+package project.SeleniumTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
